94e66966b67e03d5115c29cd3e072619d8197bd7
Rafael Liébana Fuentes <rafliefue@alum.us.es>
2021-03-20 16:59:35 +0100

    Cambios para que funcione el test (Probado en eclipse)

--- src/main/java/org/springframework/samples/petclinic/web/EncargoController.java
+++ src/main/java/org/springframework/samples/petclinic/web/EncargoController.java
@@ -1,230 +1,213 @@
+
 package org.springframework.samples.petclinic.web;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Beaver;
 import org.springframework.samples.petclinic.model.Encargo;
-import org.springframework.samples.petclinic.model.User;
-import org.springframework.samples.petclinic.repository.BeaverRepository;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 import org.springframework.samples.petclinic.service.BeaverService;
 import org.springframework.samples.petclinic.service.EncargoService;
 import org.springframework.samples.petclinic.service.UserService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
 @Controller
-//@RequestMapping("/beavers/{beaverId}")
+@RequestMapping("/beavers/{beaverId}")
 public class EncargoController {
 
-  
-    private EncargoService encargoService;
-    private UserService userService;
-    private BeaverService beaverService;
+	private EncargoService	encargoService;
+	private UserService		userService;
+	private BeaverService	beaverService;
 
 
-    @Autowired
-	public EncargoController(final EncargoService encargoService, final UserService userService,
-			final BeaverService beaverService, final AuthoritiesService authoritiesService) throws ClassNotFoundException{
+	@Autowired
+	public EncargoController(final EncargoService encargoService, final UserService userService, final BeaverService beaverService, final AuthoritiesService authoritiesService) throws ClassNotFoundException {
 		this.encargoService = encargoService;
 		this.userService = userService;
 		this.beaverService = beaverService;
 	}
 
-    @GetMapping(value = "/encargos/new")
-    public String initCreationForm(ModelMap model) {
+	@GetMapping(value = "/encargos/new")
+	public String initCreationForm(final ModelMap model) {
 
-        Beaver beaver = new Beaver();
-        Encargo encargo = new Encargo();
-        encargo.setBeaver(beaver);
-        model.addAttribute("encargo", encargo);
-        model.addAttribute("beaver", beaver);
-        return "encargos/nuevo";
+		Beaver beaver = new Beaver();
+		Encargo encargo = new Encargo();
+		encargo.setBeaver(beaver);
+		model.addAttribute("encargo", encargo);
+		model.addAttribute("beaver", beaver);
+		return "encargos/nuevo";
 
-    }
+	}
 
-    @PostMapping(value = "/encargos/new")
-    public String processCreationForm(@PathVariable("beaverId") int beaverId, @Valid Encargo encargo, BindingResult result,
-                                  ModelMap model) {
+	@PostMapping(value = "/encargos/new")
+	public String processCreationForm(@PathVariable("beaverId") final int beaverId, @Valid final Encargo encargo, final BindingResult result, final ModelMap model) {
 
-        Beaver beaver = this.beaverService.findBeaverByIntId(beaverId);
+		Beaver beaver = this.beaverService.findBeaverByIntId(beaverId);
 
-        if (result.hasErrors()) {
+		if (result.hasErrors()) {
 
-            model.addAttribute("beaver", beaver);
-            model.addAttribute("encargo", encargo);
-            return "encargos/nuevo";
-        }
-        else {
-            encargo.setBeaver(beaver);
-            this.encargoService.saveEncargo(encargo);
-            beaver.getEncargos().add(encargo);
-            this.beaverService.saveBeaver(beaver);
+			model.addAttribute("beaver", beaver);
+			model.addAttribute("encargo", encargo);
+			return "encargos/nuevo";
+		} else {
+			encargo.setBeaver(beaver);
+			this.encargoService.saveEncargo(encargo);
+			beaver.getEncargos().add(encargo);
+			this.beaverService.saveBeaver(beaver);
 
-            return "redirect:/beavers/" + beaverId;
-        }
+			return "redirect:/beavers/" + beaverId;
+		}
 
-    }
+	}
 
+	//lIST ENCARGOS
 
+	@GetMapping("/list")
+	public String listarEncargos(@PathVariable("beaverId") final int beaverId, final ModelMap model) {
 
-    //lIST ENCARGOS
+		Iterable<Encargo> encargos = this.encargoService.findEncargoByBeaverId(beaverId);
+		model.addAttribute("encargos", encargos);
+		return "encargos/listEncargos";
 
-    @GetMapping("/beavers/{beaverId}/encargos/list")
-    public String listarEncargos(@PathVariable("beaverId") int beaverId, ModelMap model) {
+	}
 
-        Iterable<Encargo> encargos = this.encargoService.findEncargoByBeaverId(beaverId);
-        model.addAttribute("encargos", encargos);
-        return "encargos/listEncargos";
+	// SHOW ENCARGOS
 
-    }
+	@GetMapping("/encargos/{encargoId}")
+	public ModelAndView mostrarEncargo(@PathVariable("encargoId") final int encargoId) {
 
-    // SHOW ENCARGOS
+		ModelAndView vista = new ModelAndView("encargos/encargosDetails");
+		Encargo encargo = new Encargo();
+		Optional<Encargo> enC = this.encargoService.findEncargoById(encargoId);
+		if (enC.isPresent()) {
+			encargo = enC.get();
+		}
 
-    @GetMapping("/encargos/{encargoId}")
-    public ModelAndView mostrarEncargo(@PathVariable("encargoId") int encargoId){
+		vista.addObject("encargo", encargo);
 
-        ModelAndView vista = new ModelAndView("encargos/encargosDetails");
-        Encargo encargo = new Encargo();
-        Optional<Encargo> enC = this.encargoService.findEncargoById(encargoId);
-        if(enC.isPresent()){
-            encargo = enC.get();
-        }
+		return vista;
+	}
 
-        vista.addObject("encargo", encargo);
+	// CREATE DE OTRA FORMA
 
-        return vista;
-    }
+	/*
+	 * @GetMapping("/encargos/nuevo")
+	 * public String initCreationForm(Beaver beaver, ModelMap model) {
+	 *
+	 * Encargo encargo = new Encargo();
+	 * model.addAttribute("encargo", encargo);
+	 * return "encargos/nuevo";
+	 * }
+	 *
+	 * @PostMapping("encargos/nuevo")
+	 * public String processCreationForm(Beaver beaver, @Valid Encargo encargo, BindingResult result,
+	 * ModelMap model) {
+	 *
+	 * if (result.hasErrors()) {
+	 * model.addAttribute("encargo", encargo);
+	 * return "encargos/nuevo";
+	 * } else {
+	 *
+	 *
+	 * if (!imagen.isEmpty()) {
+	 *
+	 * Path directorioImagen = Paths.get("src/main/resources/static/resources/images/imagenes");
+	 *
+	 * String rutaAbsoluta = directorioImagen.toFile().getAbsolutePath();
+	 * try {
+	 * byte[] bytesImg = imagen.getBytes();
+	 * Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
+	 * Files.write(rutaCompleta, bytesImg);
+	 *
+	 * encargo.setPhoto(imagen.getOriginalFilename());
+	 *
+	 * } catch (IOException e) {
+	 * e.printStackTrace();
+	 *
+	 * }
+	 *
+	 * }
+	 *
+	 *
+	 * encargo.setBeaver(beaver);
+	 * //beaver.getEncargos().add(encargo);
+	 * beaver.getEncargos();
+	 * this.encargoService.saveEncargo(encargo);
+	 *
+	 * return "redirect:/beavers/{beaverId}";
+	 * }
+	 *
+	 *
+	 *
+	 * }
+	 *
+	 */
 
-    // CREATE DE OTRA FORMA
+	//UPDATE DE ENCARGO
 
-    /*
-    @GetMapping("/encargos/nuevo")
-    public String initCreationForm(Beaver beaver, ModelMap model) {
+	/*
+	 *
+	 * @GetMapping(value = "/encargos/{encargoId}/edit")
+	 * public String initUpdateForm(@PathVariable("encargoId") int encargoId, ModelMap model) {
+	 *
+	 * Encargo encargo = new Encargo();
+	 * Optional<Encargo> enC = this.encargoService.findEncargoById(encargoId);
+	 * if (enC.isPresent()) {
+	 * encargo = enC.get();
+	 * }
+	 * model.addAttribute("encargo", encargo);
+	 * return "vista form";
+	 * }
+	 *
+	 *
+	 * @PostMapping(value = "/encargos/{encargoId}/edit")
+	 * public String processUpdateForm(@Valid Encargo encargo, BindingResult result, Beaver beaver,@PathVariable("encargoId") int encargoId, ModelMap model) {
+	 * if (result.hasErrors()) {
+	 * model.addAttribute("encargo", encargo);
+	 * return "vista form";
+	 * }
+	 * else {
+	 *
+	 * Encargo encargo1 = this.encargoService.saveEncargo(encargo);
+	 * model.addAttribute("encargo", encargo);
+	 *
+	 *
+	 * return "redirect:/beavers/{beaverId}";
+	 * }
+	 * }
+	 *
+	 *
+	 */
 
-        Encargo encargo = new Encargo();
-        model.addAttribute("encargo", encargo);
-        return "encargos/nuevo";
-    }
+	//Delete Encargo
 
-    @PostMapping("encargos/nuevo")
-    public String processCreationForm(Beaver beaver, @Valid Encargo encargo, BindingResult result,
-                                      ModelMap model) {
+	@RequestMapping(value = "/encargos/delete")
+	public String deleteEncargo(@RequestParam("encargoId") final int encargoId) {
 
-        if (result.hasErrors()) {
-            model.addAttribute("encargo", encargo);
-            return "encargos/nuevo";
-        } else {
+		Optional<Encargo> p = this.encargoService.findEncargoById(encargoId);
+		if (p.isPresent()) {
+			Encargo encargo = p.get();
+			Beaver b = encargo.getBeaver();
+			b.getEncargos().removeIf(x -> encargo.getId() == encargoId);
+			this.beaverService.saveBeaver(b);
+			this.encargoService.deleteEncargoById(encargoId);
+			return "encargos/todoOk";
 
+		} else {
+			return "exception";
+		}
 
-            if (!imagen.isEmpty()) {
-
-                Path directorioImagen = Paths.get("src/main/resources/static/resources/images/imagenes");
-
-                String rutaAbsoluta = directorioImagen.toFile().getAbsolutePath();
-                try {
-                    byte[] bytesImg = imagen.getBytes();
-                    Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
-                    Files.write(rutaCompleta, bytesImg);
-
-                    encargo.setPhoto(imagen.getOriginalFilename());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-
-                }
-
-            }
-
-
-            encargo.setBeaver(beaver);
-            //beaver.getEncargos().add(encargo);
-            beaver.getEncargos();
-            this.encargoService.saveEncargo(encargo);
-
-            return "redirect:/beavers/{beaverId}";
-        }
-
-
-
-    }
-
-    */
-
-
-
-
-
-
-    //UPDATE DE ENCARGO
-
-    /*
-
-    @GetMapping(value = "/encargos/{encargoId}/edit")
-    public String initUpdateForm(@PathVariable("encargoId") int encargoId, ModelMap model) {
-
-        Encargo encargo = new Encargo();
-        Optional<Encargo> enC = this.encargoService.findEncargoById(encargoId);
-        if (enC.isPresent()) {
-            encargo = enC.get();
-        }
-        model.addAttribute("encargo", encargo);
-        return "vista form";
-    }
-
-
-    @PostMapping(value = "/encargos/{encargoId}/edit")
-    public String processUpdateForm(@Valid Encargo encargo, BindingResult result, Beaver beaver,@PathVariable("encargoId") int encargoId, ModelMap model) {
-        if (result.hasErrors()) {
-            model.addAttribute("encargo", encargo);
-            return "vista form";
-        }
-        else {
-
-            Encargo encargo1 = this.encargoService.saveEncargo(encargo);
-            model.addAttribute("encargo", encargo);
-
-
-            return "redirect:/beavers/{beaverId}";
-        }
-    }
-
-
-     */
-
-    //Delete Encargo
-
-    @RequestMapping(value = "/encargos/delete")
-    public String deleteEncargo(@RequestParam("encargoId") int encargoId) {
-
-        Optional<Encargo> p = this.encargoService.findEncargoById(encargoId);
-        if (p.isPresent()) {
-                Encargo encargo = p.get();
-                Beaver b = encargo.getBeaver();
-                b.getEncargos().removeIf(x -> encargo.getId() == encargoId);
-                this.beaverService.saveBeaver(b);
-                this.encargoService.deleteEncargoById(encargoId);
-                return "encargos/todoOk";
-
-        }
-        else {
-            return "exception";
-        }
-
-    }
+	}
 
 }
--- src/test/java/org/springframework/samples/petclinic/web/EncargoControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/EncargoControllerTests.java
@@ -1,13 +1,12 @@
+
 package org.springframework.samples.petclinic.web;
 
-
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,183 +14,112 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Beaver;
 import org.springframework.samples.petclinic.model.User;
-import org.springframework.samples.petclinic.model.Authorities;
 import org.springframework.samples.petclinic.service.AuthoritiesService;
 import org.springframework.samples.petclinic.service.BeaverService;
 import org.springframework.samples.petclinic.service.EncargoService;
 import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = EncargoController.class,
-    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
-    excludeAutoConfiguration= SecurityConfiguration.class)
+@WebMvcTest(controllers = EncargoController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 public class EncargoControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc				mockMvc;
 
-    @MockBean
-    private EncargoService encargoService;
+	@MockBean
+	private EncargoService		encargoService;
 
-    @MockBean
-    private BeaverService beaverService;
+	@MockBean
+	private BeaverService		beaverService;
 
-    @MockBean
-    private UserService userService;
+	@MockBean
+	private UserService			userService;
 
-    @MockBean
-    private AuthoritiesService authoritiesService;
+	@MockBean
+	private AuthoritiesService	authoritiesService;
 
-    @Autowired
-    private EncargoController encargoController;
+	@Autowired
+	private EncargoController	encargoController;
 
-    private static final int TEST_BEAVER_ID = 1;
-    private static final int TEST_ENCARGO_ID = 1;
-    private static final String TEST_BEAVERUSERNAME = "beaver2";
+	private static final int	TEST_BEAVER_ID		= 1;
+	private static final int	TEST_ENCARGO_ID		= 1;
+	private static final String	TEST_BEAVERUSERNAME	= "beaver1";
 
-    private Beaver beaver1;
-    private User beaverUser1;
-    private Authorities	authorities;
-
-    @BeforeEach
-    void setup() {
-    
-        beaver1 = new Beaver();
-        beaver1.setId(TEST_BEAVER_ID);
-        beaver1.setFirstName("Nombre");
-        beaver1.setLastName("Apellidos");
-        beaver1.setEmail("valid@gmail.com");
-        beaver1.setEspecialidades("Pintura");
-        beaver1.setDni("12345678Q");
-
-        beaverUser1 = new User();
-        beaverUser1.setUsername(TEST_BEAVERUSERNAME);
-        beaverUser1.setPassword("supersecretpass");
-        beaverUser1.setEnabled(true);
-
-        beaver1.setUser(this.beaverUser1);
-        beaver1.getUser().setEnabled(true);
-
-        authorities = new Authorities();
-        authorities.setUser(beaverUser1);
-        authorities.setAuthority("beaver");
-
-        BDDMockito.given(this.userService.findUserByUsername(TEST_BEAVERUSERNAME)).willReturn(beaverUser1);
-        BDDMockito.given(this.beaverService.findBeaverByIntId(TEST_BEAVER_ID)).willReturn(beaver1);
-        BDDMockito.given(this.beaverService.findBeaverByIntId(BDDMockito.anyInt())).willReturn(beaver1);
-
-    }
+	private Beaver				beaver1;
 
 
+	@BeforeEach
+	void setup() {
 
-    @Test
-    @WithMockUser("spring")
-    public void listarEncargosTest() throws Exception {
+		User user = new User();
+		user.setUsername("beaver1");
+		user.setPassword("supersecretpass");
+		user.setEnabled(true);
 
-       /*  Beaver beaver2 = new Beaver();
-        beaver2.setId(1);
-        beaver2.setFirstName("Nombre");
-        beaver2.setLastName("Apellidos");
-        beaver2.setEmail("valid@gmail.com");
-        beaver2.setEspecialidades("Pintura");
-        beaver2.setDni("12345678Q");
+		this.beaver1 = new Beaver();
+		this.beaver1.setId(EncargoControllerTests.TEST_BEAVER_ID);
+		this.beaver1.setFirstName("beaver1");
+		this.beaver1.setId(1);
+		this.beaver1.setLastName("Apellidos");
+		this.beaver1.setEmail("valid@gmail.com");
+		this.beaver1.setEspecialidades("Pintura");
+		this.beaver1.setDni("12345678Q");
+		this.beaver1.setUser(user);
 
-        User beaver2User = new User();
-        beaver2User.setUsername("beaver2");
-        beaver2User.setPassword("superpasssecret");
-        beaver2User.setEnabled(true);
-        beaver2User.setNombre(beaver2.getFirstName());
-        beaver2User.setApellidos(beaver2.getLastName());
-        beaver2User.setNombre(beaver2.getFirstName());
-        beaver2User.setApellidos(beaver2.getLastName()); */
+		BDDMockito.given(this.userService.findUserByUsername("Apellidos")).willReturn(user);
+		BDDMockito.given(this.beaverService.findBeaverByIntId(EncargoControllerTests.TEST_BEAVER_ID)).willReturn(this.beaver1);
+		BDDMockito.given(this.beaverService.findBeaverByIntId(ArgumentMatchers.anyInt())).willReturn(this.beaver1);
 
-        mockMvc.perform(get("/beavers/{beaverId}/list",TEST_BEAVER_ID)).andExpect(status().isOk());
-    }
+	}
 
+	@WithMockUser("beaver1")
+	@Test
+	public void listarEncargosTest() throws Exception {
 
-    /*
-    @WithMockUser(username = "testuser")
-    @Test
-    public void testInitAñadirComentarioHasErrors() throws Exception {
-        mockMvc.perform(get("/beaver/{beaverId}/encargos/nuevo", TEST_BEAVER_ID)).andExpect(status().is4xxClientError());
-    }
-*/
+		System.out.println(this.beaver1.getId());
+		System.out.println(this.beaver1.getFirstName());
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/beavers/{beaverId}/list", EncargoControllerTests.TEST_BEAVER_ID)).andExpect(MockMvcResultMatchers.status().isOk());
 
+	}
 
+	@WithMockUser(value = "testuser")
+	@Test
+	public void testProcessCreationSuccess() throws Exception {
 
-    @WithMockUser(value = "spring")
-    @Test
-    public void testInitCreationSucces() throws Exception {
-        Beaver beaver2 = new Beaver();
-        beaver2.setId(1);
-        beaver2.setFirstName("Nombre");
-        beaver2.setLastName("Apellidos");
-        beaver2.setEmail("valid@gmail.com");
-        beaver2.setEspecialidades("Pintura");
-        beaver2.setDni("12345678Q");
+		Mockito.when(this.beaverService.findBeaverByIntId(ArgumentMatchers.anyInt())).thenReturn(this.beaver1);
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.post("/beavers/{beaverId}/encargos/new", EncargoControllerTests.TEST_BEAVER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("titulo", "Encargo Pinturas").param("precio", "35.50")
+				.param("disponibilidad", "true").param("descripcion", "Descripción del encargo de las pinturas del nuevo Beaver a 35 euros")
+				.param("photo", "https://previews.123rf.com/images/max5799/max57991508/max5799150800006/44259458-paisaje-de-la-pintura-al-%C3%B3leo-ramo-de-flores-en-el-fondo-del-mar-mediterr%C3%A1neo-cerca-de-las-monta%C3%B1as-oast.jpg")
+				.param("beaver.id", Integer.toString(EncargoControllerTests.TEST_BEAVER_ID)))
+			/*
+			 * .param("beaver.email", this.beaver1.getEmail())
+			 * .param("beaver.especialidades", "test@gmail.com")
+			 * .param("beaver.dni", "50506312Z")
+			 * .param("beaver.person.firstname", "TestName")
+			 * .param("beaver.person.lastname", "TestLastName")
+			 * .param("beaver.user.username", "beaver2")
+			 * .param("beaver.user.password", "superpasssecret")
+			 * .param("beaver.user.enabled", "true"))
+			 */
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection()).andExpect(MockMvcResultMatchers.view().name("redirect:/beavers/" + EncargoControllerTests.TEST_BEAVER_ID));
+	}
 
-        User beaver2User = new User();
-        beaver2User.setUsername("beaver2");
-        beaver2User.setPassword("superpasssecret");
-        beaver2User.setEnabled(true);
-        beaver2User.setNombre(beaver2.getFirstName());
-        beaver2User.setApellidos(beaver2.getLastName());
-        
-        mockMvc.perform(get("/encargos/new")).andExpect(status().isOk())
-            .andExpect(view().name("encargos/nuevo")).andExpect(model().attributeExists("encargo"));
-    }
-
-
-    @WithMockUser(value = "testuser")
-    @Test
-    public void testProcessCreationSuccess() throws Exception {
-
-        BDDMockito.given(this.userService.findUserByUsername(TEST_BEAVERUSERNAME)).willReturn(this.beaverUser1);
-        BDDMockito.when(this.beaverService.findBeaverByIntId(BDDMockito.anyInt())).thenReturn(beaver1);
-        mockMvc.perform(post("/beavers/{beaverId}/encargos/new", TEST_BEAVER_ID).with(csrf())
-            .param("titulo", "Encargo Pinturas")
-            .param("precio", "35.50")
-            .param("disponibilidad", "true")
-            .param("descripcion", "Descripción del encargo de las pinturas del nuevo Beaver a 35 euros")
-            .param("photo", "https://previews.123rf.com/images/max5799/max57991508/max5799150800006/44259458-paisaje-de-la-pintura-al-%C3%B3leo-ramo-de-flores-en-el-fondo-del-mar-mediterr%C3%A1neo-cerca-de-las-monta%C3%B1as-oast.jpg")
-            .param("beaver.id", Integer.toString(TEST_BEAVER_ID)))
-            /* .param("beaver.email", this.beaver1.getEmail())
-            .param("beaver.especialidades", "test@gmail.com")
-            .param("beaver.dni", "50506312Z")
-            .param("beaver.person.firstname", "TestName")
-            .param("beaver.person.lastname", "TestLastName")
-            .param("beaver.user.username", "beaver2")
-            .param("beaver.user.password", "superpasssecret")
-            .param("beaver.user.enabled", "true")) */
-            .andExpect(status().is3xxRedirection())
-            .andExpect(view().name("redirect:/beavers/" + TEST_BEAVER_ID));
-    }
-
-
-
-    @WithMockUser(value = "testuser")
-    @Test
-    public void testProcessCreationFormHasErrors() throws Exception {
-        mockMvc.perform(post("/beavers/{beaverId}/encargos/nuevo", TEST_BEAVER_ID).with(csrf())
-            .param("titulo", "Encargo Pinturas")
-            .param("precio", "40.0")
-            .param("disponibilidad", "true")
-            .param("descripcion", "Descripción del encargo de las pinturas del nuevo Beaver a 35 euros"))
-            //.param("photo", "https://previews.123rf.com/images/max5799/max57991508/max5799150800006/44259458-paisaje-de-la-pintura-al-%C3%B3leo-ramo-de-flores-en-el-fondo-del-mar-mediterr%C3%A1neo-cerca-de-las-monta%C3%B1as-oast.jpg"))
-            //.andExpect(model().attributeHasErrors("encargo"))
-            //.andExpect(model().attributeHasFieldErrors("encargo", "precio"))
-            .andExpect(status().isOk())
-            .andExpect(view().name("encargos/nuevo"));
-    }
-
+	@WithMockUser(value = "testuser")
+	@Test
+	public void testProcessCreationFormHasErrors() throws Exception {
+		this.mockMvc
+			.perform(MockMvcRequestBuilders.post("/beavers/{beaverId}/encargos/nuevo", EncargoControllerTests.TEST_BEAVER_ID).with(SecurityMockMvcRequestPostProcessors.csrf()).param("titulo", "Encargo Pinturas").param("precio", "40.0")
+				.param("disponibilidad", "true").param("descripcion", "Descripción del encargo de las pinturas del nuevo Beaver a 35 euros"))
+			//.param("photo", "https://previews.123rf.com/images/max5799/max57991508/max5799150800006/44259458-paisaje-de-la-pintura-al-%C3%B3leo-ramo-de-flores-en-el-fondo-del-mar-mediterr%C3%A1neo-cerca-de-las-monta%C3%B1as-oast.jpg"))
+			//.andExpect(model().attributeHasErrors("encargo"))
+			//.andExpect(model().attributeHasFieldErrors("encargo", "precio"))
+			.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("encargos/nuevo"));
+	}
 
 }
