6cc10d45cc05f9f0c46403e69c01b23899416327
PedroCoza <56022203+PedroCoza@users.noreply.github.com>
2021-03-21 19:04:39 +0100

    Update EncargoServiceTests.java

--- src/test/java/org/springframework/samples/petclinic/service/EncargoServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/EncargoServiceTests.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
