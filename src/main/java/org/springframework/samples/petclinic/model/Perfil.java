e57aeb7091abf195939b6127cd1de252e0a21702
Manu <manrubben@alum.us.es>
2021-03-24 18:41:22 +0100

    Interface Perfil fixed

--- src/main/java/org/springframework/samples/petclinic/model/Perfil.java
+++ src/main/java/org/springframework/samples/petclinic/model/Perfil.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
@@ -15,13 +16,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table (name = "portfolio")
+@Table (name = "perfil")
 public class Perfil extends BaseEntity{
 
     private String descripcion;
 
     @ElementCollection(targetClass=String.class)
-    private Collection<String> photos;
+    private Collection<String> portfolio;
+
+    @OneToOne
+    private Beaver beaver;
 
     /*
 
