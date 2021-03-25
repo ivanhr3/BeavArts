d0a9658807c74b4e3a722f4679a6b80260d81604
Manu <manrubben@alum.us.es>
2021-03-25 18:31:33 +0100

    BeaverController done

--- src/main/java/org/springframework/samples/petclinic/web/BeaverController.java
+++ src/main/java/org/springframework/samples/petclinic/web/BeaverController.java
@@ -1,5 +1,88 @@
 package org.springframework.samples.petclinic.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Beaver;
+import org.springframework.samples.petclinic.model.Perfil;
+import org.springframework.samples.petclinic.model.User;
+import org.springframework.samples.petclinic.service.BeaverService;
+import org.springframework.samples.petclinic.service.PerfilService;
+import org.springframework.samples.petclinic.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/beavers")
 public class BeaverController {
-    
+
+
+
+    @Autowired
+    private BeaverService beaverService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PerfilService perfilService;
+
+    @RequestMapping("/beaverInfo/{beaverId}")
+    public ModelAndView mostrarPerfilUsuario(@PathVariable("beaverId") final int beaverId) {
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication1.getName();
+        User user = this.userService.findUserByUsername(currentPrincipalName);
+        Beaver beaver = user.getBeaver();
+        Perfil perfil = beaver.getPerfil();
+
+        ModelAndView vista = new ModelAndView("users/perfilBeaver");
+        vista.addObject("beaver", beaver);
+        vista.addObject("perfil", perfil);
+
+        return vista;
+    }
+
+    @GetMapping("beaverInfo/perfil/edit")
+    public String initActualizarPerfil(@RequestParam int beaverId, ModelMap model) {
+        Authentication authentication1 = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication1.getName();
+        User user = this.userService.findUserByUsername(currentPrincipalName);
+
+        Beaver beaver = this.beaverService.findBeaverByIntId(beaverId);
+        String vista;
+
+        if(user.getUsername().equals(beaver.getUser().getUsername())) {
+
+            model.addAttribute("perfil", beaver.getPerfil());
+            vista = "users/editarPerfil";
+            return vista;
+
+        } else {
+
+            vista = "accesoNoAutorizado";
+        }
+
+        return vista;
+    }
+
+    @PostMapping("beaverInfo/perfil/edit")
+    public String processActualizarPerfil(@Valid Perfil perfil, BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            model.put("perfil", perfil);
+            return "users/editarPerfil";
+
+        } else {
+            Perfil perfil1 = this.perfilService.savePerfil(perfil);
+            String vista = "users/perfilBeaver";
+            model.put("perfil", perfil1);
+
+            return vista;
+        }
+    }
+
 }
