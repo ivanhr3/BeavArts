e84b8699ed7c33f9ee3373291e7f42e07baf6cc1
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-03-27 00:46:59 +0100

    Preparacion para subir a GitHub

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -3,7 +3,7 @@
 --INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
 
 --Usuarios
-INSERT INTO users(username,password,enabled) VALUES ('user1', 'user1', TRUE);
+INSERT INTO users(username,password,enabled) VALUES ('user1', '$2a$10$QaodejE88za17DUMb8pepuXAR.r8Z.NF.c7DkT7K3SAcPk8NisKTu', TRUE);
 INSERT INTO authorities(id, username, authority) VALUES(1, 'user1', 'user');
 INSERT INTO portfolio(id, sobre_mi) VALUES (1, 'Soy un usuario que intenta un poco de todo');
 INSERT INTO beavers(id, first_name, last_name, dni, email, url_foto_perfil, valoracion, portfolio_id, username) VALUES (1, 'Nombre', 'Apellidos', '12234321Q', 'emailprueba@email.com', 'urlfotoperfil.com', 4.5, 1 , 'user1');
@@ -12,7 +12,7 @@ INSERT INTO beaver_especialidades(beaver_id, especialidades) VALUES (1, "TEXTIL"
 INSERT INTO portfolio_photos(portfolio_id, photos) VALUES (1, "foto1.com");
 INSERT INTO portfolio_photos(portfolio_id, photos) VALUES (1, "foto2.com");
 
-INSERT INTO users(username,password,enabled) VALUES ('user2', 'user2', TRUE);
+INSERT INTO users(username,password,enabled) VALUES ('user2', '$2a$10$/9VLfws1dOLXMW.sB4qfWuY2s/KyyB0y2lsaxP3u1Ubh.pbn98H5u', TRUE);
 INSERT INTO authorities(id, username, authority) VALUES(2, 'user2', 'user');
 INSERT INTO portfolio(id, sobre_mi) VALUES (2, 'Soy un usuario que intenta un poco de todo');
 INSERT INTO beavers(id, first_name, last_name, dni, email, url_foto_perfil, valoracion, portfolio_id, username) VALUES (2, 'Nombre', 'Apellidos', '12234321Q', 'emailprueba@email.com', 'urlfotoperfil.com', 4.5, 2 , 'user2');
