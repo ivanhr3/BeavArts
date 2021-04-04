8cd47fc913b77868e3b164aef68051107a1df93c
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-04-04 18:51:05 +0200

    TestUsers Password Actualizada
    Cambiada contraseña de los usuarios de prueba a una más segura

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -3,7 +3,7 @@
 --INSERT INTO authorities(id,username,authority) VALUES (1,'admin1','admin');
 
 --Usuarios
-INSERT INTO users(username,password,enabled) VALUES ('user1', '$2a$10$QaodejE88za17DUMb8pepuXAR.r8Z.NF.c7DkT7K3SAcPk8NisKTu', TRUE);
+INSERT INTO users(username,password,enabled) VALUES ('user1', '$2a$10$92y/D.0vHcQtnBgATuJmPeMfCBYI08aNCcCxFV8gtKSyr/Y1zuIw.', TRUE);
 INSERT INTO authorities(id, username, authority) VALUES(1, 'user1', 'user');
 INSERT INTO portfolio(id, sobre_mi) VALUES (1, 'Soy un usuario que intenta un poco de todo');
 INSERT INTO beavers(id, first_name, last_name, dni, email, url_foto_perfil, valoracion, portfolio_id, username) VALUES (1, 'Nombre', 'Apellidos', '12234321Q', 'emailprueba@email.com', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/doraemon-borracho-1532607482.jpg', 4.5, 1 , 'user1');
@@ -12,7 +12,7 @@ INSERT INTO beaver_especialidades(beaver_id, especialidad) VALUES (1, 'ESCULTURA
 INSERT INTO portfolio_photos(portfolio_id, photos) VALUES (1, 'https://cflvdg.avoz.es/default/2014/05/13/0012_201405G13P57F1jpg/Foto/G13P57F1.jpg');
 INSERT INTO portfolio_photos(portfolio_id, photos) VALUES (1, 'https://estaticos-cdn.elperiodico.com/clip/36cd084f-1cf9-42f8-9db5-4b40ae199a92_alta-libre-aspect-ratio_default_0.jpg');
 
-INSERT INTO users(username,password,enabled) VALUES ('user2', '$2a$10$/9VLfws1dOLXMW.sB4qfWuY2s/KyyB0y2lsaxP3u1Ubh.pbn98H5u', TRUE);
+INSERT INTO users(username,password,enabled) VALUES ('user2', '$2a$10$/h5dlN5Jmeimr/lkdzt/vel9l9YWftfHqI8Opwg75fih62m0cdJSi', TRUE);
 INSERT INTO authorities(id, username, authority) VALUES(2, 'user2', 'user');
 INSERT INTO portfolio(id, sobre_mi) VALUES (2, 'Soy un usuario que intenta un poco de todo');
 INSERT INTO beavers(id, first_name, last_name, dni, email, url_foto_perfil, valoracion, portfolio_id, username) VALUES (2, 'Nombre', 'Apellidos', '12234321Q', 'emailprueba@email.com', 'https://cdn.pixabay.com/photo/2020/10/16/11/02/astronaut-5659227_960_720.png', 4.5, 2 , 'user2');
