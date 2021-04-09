a8b868da272419b4dc57bd54fe5512e3ff5d93be
adrmadfer <adrmadfer@alum.us.es>
2021-04-09 18:05:46 +0200

    fix error Especialidad

--- src/test/java/org/springframework/samples/petclinic/web/AnuncioControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/AnuncioControllerTests.java
@@ -106,7 +106,7 @@ public class AnuncioControllerTests {
         anuncio2.setDescripcion("Esto es una descripción 2");
         anuncio2.setPrecio(40.0);
         anuncio2.setTitulo("Esto es un título 2");
-        anuncio2.setEspecialidad(Especialidad.ACRÍLICO);
+        anuncio2.setEspecialidad(Especialidad.RESINA);
         anuncio2.setId(91);
 
         anuncio3 = new Anuncio();
@@ -114,7 +114,7 @@ public class AnuncioControllerTests {
         anuncio3.setDescripcion("Esto es una descripción 2");
         anuncio3.setPrecio(40.0);
         anuncio3.setTitulo("Esto es un título 2");
-        anuncio3.setEspecialidad(Especialidad.ACRÍLICO);
+        anuncio3.setEspecialidad(Especialidad.RESINA);
         anuncio3.setId(92);
 
         solicitud = new Solicitud();
